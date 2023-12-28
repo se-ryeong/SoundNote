6261af814d519aa96807996757dfc4c7b7efec2e
seryeong <seryz029@gmail.com>
2023-12-28 17:24:24 +0900

    Initial Commit

---
+++ RecordApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecordApp
+//
+//  Created by se-ryeong on 2023/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
