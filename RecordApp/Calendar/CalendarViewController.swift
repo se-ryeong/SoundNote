e20403a651ad06b7bf5b7da535f9c6af6944e3ab
seryeong <seryz029@gmail.com>
2023-12-30 22:48:00 +0900

    초기세팅

---
+++ RecordApp/Calendar/CalendarViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CalendarViewController.swift
+//  RecordApp
+//
+//  Created by se-ryeong on 2023/12/28.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+ 
+}
---
+++ RecordApp/Diary/DiaryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DiaryViewController.swift
+//  RecordApp
+//
+//  Created by se-ryeong on 2023/12/28.
+//
+
+import UIKit
+
+class DiaryViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .blue
+    }
+}
